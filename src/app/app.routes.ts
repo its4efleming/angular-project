ecfea4ba89a210b25d5ab1d946f7390384bd42c0
Emerson Fleming <efleming@its4logistics.com>
2024-03-29 10:54:57 -0700

    files

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { MembersComponent } from './members/members.component';
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [];
